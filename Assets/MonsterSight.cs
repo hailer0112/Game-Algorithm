7d38e520b91d122f8a5cb912b227769faa34a603
hailer0112 <hailer0112@gmail.com>
2026-05-27 16:11:05 +0900

    Day01,02
    게임 수학 기초 (벡터와 공간)
    게임 수학 심화 (내적/외적)

---
+++ Assets/MonsterSight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets
+{
+    public class MonsterSight : MonoBehaviour
+    {
+        public Transform player;
+        public float sightAngle = 60f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 diirToPlayer = (player.position - transform.position).normalized;
+
+            float dot = Vector3.Dot(transform.forward, diirToPlayer);
+            float angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
+
+            if (angle < sightAngle)
+            {
+                Debug.Log("플레이어 발견");
+                Vector3 cross = Vector3.Cross(transform.forward, diirToPlayer);
+
+                if (cross.y > 0)
+                {
+                    Debug.Log("플레이어는 오른쪽에 있음");
+                }
+                else if (cross.y < 0)
+                {
+                    Debug.Log("플레이어는 왼쪽에 있음");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/PracticTransform.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets
+{
+    public class PracticTransform : MonoBehaviour
+    {
+        public float moveSpeed = 5f;
+        // Use this for initialization
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector2 inputVector = Vector2.zero;
+
+            if (Keyboard.current is not null)
+            {
+                float h = 0;
+                float v = 0;
+
+                if (Keyboard.current.aKey.isPressed) h = -1;
+                if (Keyboard.current.dKey.isPressed) h = 1;
+                if (Keyboard.current.wKey.isPressed) v = 1;
+                if (Keyboard.current.sKey.isPressed) v = -1;
+
+                inputVector = new Vector2(h, v);
+            }
+            Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y).normalized;
+
+            if (moveDir.magnitude > 0)
+            {
+                transform.Translate(moveDir * moveSpeed * Time.deltaTime, Space.World);
+            }
+        }
+    }
+}
